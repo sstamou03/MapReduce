c01e1577b1a363dcffb74fd1ceae469b0a28028d
Spyros <stamousp03@gmail.com>
2026-04-23 22:36:47 +0300

    export new data functions from database package

--- database/__init__.py
+++ database/__init__.py
@@ -1,10 +1,10 @@
 from .db import get_db, get_minio, Job, Task, JobStatus, TaskStatus, TaskType
 from .crud import create_job, get_job, get_all_jobs, get_jobs_for_user
 from .crud import update_job_status, delete_job, update_job_output_ref
-from .crud import create_task, get_tasks_for_job, update_task_status
+from .crud import create_task, get_tasks_for_job, update_task_status, delete_user_jobs
 from .schemas import JobCreate, JobResponse, JobDetailResponse
 from .schemas import TaskResponse, TaskStatusUpdate, FileUploadResponse
-from .storage import split_and_upload_input
+from .storage import split_and_upload_input, shuffle_intermediate_results, delete_job_files
 
 
  
